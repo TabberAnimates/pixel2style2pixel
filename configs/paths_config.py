0f68018c86126140b26db914f4f4696e280e1d3f
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-03-29 19:04:16 -0400

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,11 +1,6 @@
 dataset_paths = {
-	'celeba_train': '',
-	'celeba_test': '',
-	'celeba_train_sketch': '',
-	'celeba_test_sketch': '',
-	'celeba_train_segmentation': '',
-	'celeba_test_segmentation': '',
-	'ffhq': '',
+    'ffhq': '/path/to/ffhq/images256x256'
+    'celeba_test': '/path/to/CelebAMask-HQ/test_img',
 }
 
 model_paths = {
